0f542dee117d50a098c86b6590dd6dfa80addc70
Ümit Tengiz <36554203+umittengiz@users.noreply.github.com>
2022-10-22 03:20:43 +0300

    Initial commit

---
+++ src/views/market.tsx
@@ -0,0 +1,41 @@
+import '@/styles/market/index.scss';
+import { useMarket } from '@/store/market';
+
+export default defineComponent({
+  name: 'Markets',
+  async setup() {
+    const marketStore = useMarket();
+    await marketStore.getList();
+    const { fruitList } = marketStore;
+
+    return { fruitList };
+  },
+  render() {
+    return (
+      <div>
+        <h3>FruitList</h3>
+        <table class="table">
+          <thead>
+            <tr>
+              <th class="c-#67c23a">ID</th>
+              <th class="c-#e6a23c">Name</th>
+              <th class="c-#79bbff">Price</th>
+            </tr>
+          </thead>
+
+          <tbody>
+            {this.fruitList.map((item) => {
+              return (
+                <tr key={item.id}>
+                  <td class="c-#67c23a">{item.id}</td>
+                  <td class="c-#e6a23c">{item.name}</td>
+                  <td class="c-#79bbff">${item.price}</td>
+                </tr>
+              );
+            })}
+          </tbody>
+        </table>
+      </div>
+    );
+  }
+});
